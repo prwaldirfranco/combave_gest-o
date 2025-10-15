96dac0ae4b444df9d9467f36310c147d7655d674
WALDIR FRANCO MESQUITA JUNIOR <prwaldirfranco@gmail.com>
2025-10-15 07:56:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import login
+from login import login
 from menu import mostrar_menu
 
 st.set_page_config(page_title="Sistema Igreja", layout="wide", initial_sidebar_state="expanded")
